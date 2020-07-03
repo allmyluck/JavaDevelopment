fbe68340c0322799060037c41623055f1f68501d
mozerfucker <tokarevnikita08@gmail.com>
2020-07-03 13:13:51 +0300

    Create tables: authors and news(localhost:3306)

--- Database/src/com/company/Main.java
+++ Database/src/com/company/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         Scanner in = new Scanner(System.in);
         System.out.println("Please name user: ");
         String name = in.nextLine();
@@ -12,8 +12,16 @@ public class Main {
         String password = in.nextLine();
         System.out.println("Please database name's: ");
         String dbName = in.nextLine();
+        String firstDB = "CREATE TABLE authors(id INT NOT NULL AUTO_INCREMENT, name varchar(255) NOT NULL, PRIMARY KEY(id));";
+        String secondDB = "CREATE TABLE news(id INT PRIMARY KEY NOT NULL AUTO_INCREMENT," +
+                " idAuthor INT NOT NULL," +
+                " title TEXT, description TEXT, puDate DATETIME, link TEXT," +
+                " FOREIGN KEY (idAuthor)  REFERENCES authors(id));";
         DatabaseHandler handler = new DatabaseHandler(dbName, name, password);
-        //Connection conn = handler.ConnectDatabase();
-        //Statement st = handler.GetStatement();
+        handler.GetConnect();
+        Statement st = handler.GetStatement();
+        st.executeUpdate(firstDB);
+        st.executeUpdate(secondDB);
+
     }
 }
